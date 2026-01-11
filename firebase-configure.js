b53a98233ea318e866cac3d678002f9ca6eb6d45
DGsellBOT <stanix579@gmail.com>
2026-01-11 08:07:43 +0530

    Update firebase-configure.js

--- firebase-configure.js
+++ firebase-configure.js
@@ -10,10 +10,34 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 const auth = firebase.auth();
 const db = firebase.firestore();
-const storage = firebase.storage();
 
 // Admin UID
 const ADMIN_UID = "PKSeq0uhBQcJwEjYCW085jtV24e2";
+
+// Auth state listener
+auth.onAuthStateChanged((user) => {
+  const currentPage = window.location.pathname.split('/').pop();
+  
+  if (user) {
+    // User is logged in
+    if (currentPage === 'login.html' || currentPage === 'signup.html' || currentPage === 'index.html') {
+      // Redirect to dashboard based on role
+      if (user.uid === ADMIN_UID) {
+        window.location.href = 'admin.html';
+      } else {
+        window.location.href = 'dashboard.html';
+      }
+    }
+  } else {
+    // User is logged out
+    if (currentPage === 'dashboard.html' || currentPage === 'admin.html') {
+      window.location.href = 'login.html';
+    }
+  }
+});
